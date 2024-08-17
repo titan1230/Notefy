0a2ac1d7f676cc0272ef6aff55086fef100b7fc0
Akshat Jain <akshat00jain@gmail.com>
2024-08-17 19:21:43 +0530

    feat: Add Navbar.server component

---
+++ src/components/Navbar.server.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { onGetUserAction } from '@/app/actions/NavGetUser';
+import NavbarContent from '@/components/NavbarContent';
+
+export default async function Navbar() {
+    const userExists = await onGetUserAction();
+
+    return (
+        <div className='pr-4 pt-4 pl-4 '>
+            <div className="navbar glass rounded-xl">
+                <div className="navbar-start">
+                    <p className="text-xl text-black pl-2 font-Grey_Qo">Notefy</p>
+                </div>
+                <NavbarContent userExists={userExists} />
+            </div>
+        </div>
+    );
+}
