0a1886f34e69e0c6e0671f2d493dcd2ee67956b1
Akshat Jain <akshat00jain@gmail.com>
2024-08-18 01:53:15 +0530

    refactor: Update auth.ts to add more features

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,33 @@
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import client from "@/lib/db"
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import client from "@/lib/db";
 import authConfig from "./auth.config";
 
 export const BASE_PATH = "/api/auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: MongoDBAdapter(client),
+  adapter: MongoDBAdapter(client, { databaseName: "users" }),
+  jwt: {
+    maxAge: 30 * 24 * 60 * 60,
+  },
   callbacks: {
+    jwt({ token, user, account }) {
+
+      if (user) {
+
+        token.id = user.id;
+
+        if (account) {
+          token.accessToken = account.access_token;
+          token.provider = account.provider;
+        }
+      }
+
+      return token;
+    },
     authorized({ request, auth }) {
       try {
         const { pathname } = request.nextUrl;
-
         const unprotectedRoutes = ["/", "/onboarding", "/api/auth/signin"];
 
         if (unprotectedRoutes.includes(pathname)) {
@@ -26,24 +41,61 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
     },
     async session({ session, token }) {
+
       if (token?.accessToken) {
         session.accessToken = token.accessToken as string;
+
+        try {
+          const db = client.db("users");
+          const collection = db.collection("users");
+
+          const user = await collection.findOne({ email: token.email });
+          session.user.id = user?._id.toString() as string;
+
+          if (user?.username) {
+            session.user.username = user.username;
+          } else {
+            console.log("User not found, creating new user");
+            const newUser = {
+              username: token.email?.substring(0, token.email.indexOf("@")) || "defaultUsername",
+            };
+
+            const insertResult = await collection.findOneAndUpdate(
+              { email: token.email },                  // Filter by email
+              { $set: newUser },                       // Update with new user data
+              { returnDocument: 'after', upsert: true } // Return the updated document after upserting
+            );
+
+            session.user.username = newUser.username;
+          }
+        } catch (error) {
+          console.error("Failed to fetch or insert user:", error);
+        }
       }
+
       return session;
     },
   },
   basePath: BASE_PATH,
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
   },
   secret: process.env.AUTH_SECRET,
   ...authConfig,
-})
+});
 
 declare module "next-auth" {
   interface Session {
-    accessToken?: string
+    accessToken?: string;
+    user: {
+      id?: string;
+      name?: string;
+      email?: string;
+      image?: string;
+      username?: string;
+    };
   }
 }
 
-export const { GET, POST } = handlers
+export const { GET, POST } = handlers;
