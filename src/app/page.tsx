6166bed7fe7ed18a587cc7e9efd053d64667c2ad
Akshat Jain <akshat00jain@gmail.com>
2024-08-17 19:54:01 +0530

    refactor: Update Navbar import in page.tsx to use server-side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import LandingAnimation from "@/components/LandingAnimation";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar.server";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen home-container no-scrollbar scrollbar-hide">
-      <main className="flex-grow bg-base-100 no-scrollbar scrollbar-hide">
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow bg-base-100">
         <Navbar />
-        <section className="hero py-20 flex flex-col md:flex-row items-center text-white no-scrollbar scrollbar-hide">
-          <div className="container mx-auto flex flex-col md:flex-row items-center scrollbar-hide">
+        <section className="hero py-20 flex flex-col md:flex-row items-center text-white">
+          <div className="container mx-auto flex flex-col md:flex-row items-center ">
+
             {/* Left side: Text */}
             <div className="md:w-1/2 text-left">
               <h1 className="text-4xl md:text-6xl font-bold mb-4">
@@ -16,12 +17,12 @@ export default function Home() {
               <p className="text-lg md:text-2xl mb-8">
                 Capture, organize, and manage your notes effortlessly with Notefy, your all-in-one tool for staying productive and on top of your tasks.
               </p>
-              <a href="/onboard" className="btn btn-info text-xl py-3 px-6 rounded-lg">
+              <a href="/onboarding" className="btn btn-info text-xl py-3 px-6 rounded-lg">
                 Get Started for Free
               </a>
             </div>
 
-            {/* Right side: Image or Lottie */}
+            {/* Right side: Lottie */}
             <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center">
               <LandingAnimation />
             </div>
