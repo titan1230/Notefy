89f1e7b550bc235f86be1d5c01b87675d4629c1d
Akshat Jain <akshat00jain@gmail.com>
2024-08-18 21:10:12 +0530

    refactor: Update PUT route to update note content

--- src/app/api/note/[noteID]/route.ts
+++ src/app/api/note/[noteID]/route.ts
@@ -4,6 +4,12 @@ import { ObjectId } from "mongodb";
 
 interface ParamInterface {
     noteID: string | ObjectId;
+    content: string;
+}
+
+interface NoteInterface {
+    content: string;
+    editorID: string;
 }
 
 export async function GET(req: NextRequest, { params }: { params: ParamInterface }) {
@@ -33,5 +39,40 @@ export async function GET(req: NextRequest, { params }: { params: ParamInterface
         return NextResponse.json(note, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: "Failed to fetch notes" }, { status: 500 });
+    }
+}
+
+export async function PUT(req: NextRequest, { params }: { params: ParamInterface }) {
+    try {
+        const { noteID } = params;
+        const { content, editorID }: NoteInterface = await req.json();
+
+        let objectId;
+        try {
+            objectId = new ObjectId(noteID);
+        } catch (err) {
+            return NextResponse.json({ error: "Invalid note ID" }, { status: 400 });
+        }
+
+        const db = DBclient.db("notes");
+        const collection = db.collection("notes");
+
+        const note = await collection.findOne({ _id: objectId });
+
+        if (!note) {
+            return NextResponse.json({ error: "Note not found" }, { status: 404 });
+        }
+
+        console.log(note.creatorID.toString(), editorID);
+        if (note.creatorID.toString() !== editorID) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+        }
+
+        await collection.updateOne({ _id: objectId }, { $set: {body: content} }, { upsert: true });
+
+        return NextResponse.json({ message: "Note updated" }, { status: 200 });
+    } catch (error) {
+        console.error("Error updating note:", error);
+        return NextResponse.json({ error: "Failed to update note" }, { status: 500 });
     }
 }
