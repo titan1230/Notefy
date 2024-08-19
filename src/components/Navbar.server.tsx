377fc5a651a1d07f2397e3b73b16cf6865f3d99d
Akshat Jain <akshat00jain@gmail.com>
2024-08-19 11:35:04 +0530

    refactor: Update Navbar component to include Notefy logo and improve styling

--- src/components/Navbar.server.tsx
+++ src/components/Navbar.server.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { onGetUserAction } from '@/actions/GetUser';
 import NavbarContent from '@/components/NavbarContent';
+import Image from 'next/image';
 
 export default async function Navbar() {
     const userExists = await onGetUserAction();
@@ -9,7 +10,10 @@ export default async function Navbar() {
         <div className='pr-4 pt-4 pl-4 '>
             <div className="navbar glass rounded-xl">
                 <div className="navbar-start">
-                    <p className="text-xl text-black pl-2 font-Grey_Qo">Notefy</p>
+                    <p className="text-xl text-black pl-2 font-Grey_Qo flex">
+                        <Image src="/favicon.ico" alt="Notefy Logo" width={30} height={30}  className='mr-5'/>
+                        Notefy 
+                    </p>
                 </div>
                 <NavbarContent userExists={userExists} />
             </div>
