60e79b4f48812f5bdf035cf63aafb2a62dc9cb22
Akshat Jain <akshat00jain@gmail.com>
2024-08-19 17:43:35 +0530

    refactor: Lazy load NoteEditor component for improved performance

--- src/app/(pages)/note/new/page.tsx
+++ src/app/(pages)/note/new/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Suspense } from 'react';
+import NewNoteEditor from '@/components/TipTap/NewNoteEditor';
+import { auth } from '@/auth';
 
-const NewNotePage = () => {
+const NoteEditor = async () => {
+
+  const session = await auth();
+
   return (
-    <div>NewNotePage</div>
-  )
-}
+    <Suspense
+      fallback={
+        <div className="relative min-h-screen bg-[#101720]">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <span className="loading loading-spinner text-secondary"></span>
+          </div>
+        </div>
+      }
+    >
+      <NewNoteEditor editorID={session?.user?.id!} />
+    </Suspense>
+  );
+};
 
-export default NewNotePage;
+export default NoteEditor;
